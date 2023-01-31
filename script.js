399658771027e1435f352007a1a3e65d22d6b0fc
Thoraya Elsharawy <thorayaelsharawy@gmail.com>
2023-01-31 21:08:08 +0000

    Handle blurring functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+
+let interval = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(interval);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = 1 - (load / 100);
+
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
